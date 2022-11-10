d15a409a537f25d36063a18c651236018d0aacc2
Pankaj Vaishnav <97958495+pankaj-vaishnaw@users.noreply.github.com>
2022-11-10 16:07:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+import Card from './Components/Card';
+import Img1 from './Images/img1.jpg';
+import Img2 from './Images/img3.jpg';
+import Img3 from './Images/img4.jpg';
+import Img4 from './Images/img2.jpg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Card movie="Ironman" yearAndAuthor="2000,Author" images={Img1} />
+      <Card
+        movie="Harry Porter"
+        yearAndAuthor="2002,Author"
+        images={Img3}
+      />
+          <Card
+        movie="Harry Porter : Animation"
+        yearAndAuthor="2004,Author"
+        images={Img2}
+      />
+          <Card
+        movie="Iron Man : Age of Ultron"
+        yearAndAuthor="2018,Author"
+        images={Img4}
+      />
+      
+    </div>
+  );
+}
+
+export default App;
